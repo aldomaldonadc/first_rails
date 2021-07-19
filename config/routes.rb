cb12b0232e8bdc9dbfc47f46f178b3635aa4c61c
Aldo Maldonado <82490718+aldomaldonadc@users.noreply.github.com>
2021-07-19 12:37:35 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   #verbo, camino, to y controller#action
+  root to: 'pages#home'
+
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'restaurants', to: 'restaurants#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
