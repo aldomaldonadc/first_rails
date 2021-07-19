a62399c4fc766d27cea159cfed03d1a084dbcc98
Aldo Maldonado <82490718+aldomaldonadc@users.noreply.github.com>
2021-07-19 12:14:19 -0400

    cambio paginas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  #verbo, camino, to y controller#action
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
